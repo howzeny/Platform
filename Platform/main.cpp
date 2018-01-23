b8f0ee34613dc979cfae8f3a551b944e7bf39cb2
남상규 <korea1628@gmail.com>
2018-01-23 13:25:03 +0900

    Initial Commit

---
+++ Platform/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Platform
+//
+//  Created by NamSangGyu on 2018. 1. 23..
+//  Copyright © 2018년 NamSangGyu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
